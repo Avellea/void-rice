4b5d346c37052ae35b4ed5c8b1119dc44daf69f8
Dynamic Donkey <dynamicdonkey0@gmail.com>
2021-01-16 08:32:22 -0600

    Added surf config and modified slstatus

--- .bashrc
+++ .bashrc
@@ -6,7 +6,7 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
-[ -f "$HOME/.config/scripts/aliasrc" ] && source "$HOME/.config/scripts/aliasrc
+[ -f "$HOME/.config/scripts/aliasrc" ] && source "$HOME/.config/scripts/aliasrc"
 
 export PS1="\[\033[38;5;26m\][\[$(tput sgr0)\]\[\033[38;5;32m\]\u\[$(tput sgr0)\]\[\033[38;5;26m\]@\[$(tput sgr0)\]\[\033[38;5;32m\]\h\[$(tput sgr0)\]\[\033[38;5;38m\] \W\[$(tput sgr0)\]\[\033[38;5;26m\]]\[$(tput sgr0)\]\\$ \[$(tput sgr0)\]"
 
--- suckless-config/slstatus/config.h
+++ suckless-config/slstatus/config.h
@@ -65,8 +65,8 @@ static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
-    //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, "[VOL %4%%] ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+    	{ netspeed_rx, "[DL %sB/s] ", "wlp6s0" },
+	//{ run_command, "[VOL %4%%] ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ cpu_perc, "[CPU %s%%] ", NULL	      },
 	{ ram_perc, "[RAM %s%%] ", NULL	      },
 	{ datetime, "%s",           "%a %b %d %r" },
